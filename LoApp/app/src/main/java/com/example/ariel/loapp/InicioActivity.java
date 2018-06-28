3a8e1a958a51ee107a794035909bcab5b91fc666
cristian <cristianmagne123@gmail.com>
2018-06-27 22:44:16 -0400

    magnus1

--- LoApp/app/src/main/java/com/example/ariel/loapp/InicioActivity.java
+++ LoApp/app/src/main/java/com/example/ariel/loapp/InicioActivity.java
@@ -1,15 +1,28 @@
 package com.example.ariel.loapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InicioActivity extends AppCompatActivity {
+
+    Button crear_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
+        crear_user= (Button) findViewById(R.id.crearuser);
+        crear_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent opci=new Intent(getApplicationContext(), FormularioActivity.class);
+                startActivity(opci);
+            }
+        });
     }
 
 }
